05c2572f6d0574aa2992e26f308cc11a1547ec7a
Ironite01 <nelsonzhongjunwei@gmail.com>
2025-11-04 16:15:16 +0800

    reward tier page

--- paths.js
+++ paths.js
@@ -16,7 +16,7 @@ export const user = {
   updatePassword: `${backendPath}/user/update-password`,
   updateProfile: (userId) => `${backendPath}/user/${userId}`,
   rewards: (userId) => `${backendPath}/user/${userId}/rewards?limit=10000`,
-  reward: (userId, userRewardId) => `${backendPath}/user/${userId}/rewards/${userRewardId}`
+  reward: (userId, userRewardId) => `${backendPath}/user/${userId}/rewards/${userRewardId}`,
 }
 
 export const admin = {
@@ -38,7 +38,12 @@ export const admin = {
 export const campaigns = {
   getById: (campaignId) => `${backendPath}/campaigns/${campaignId}`,
   get: `${backendPath}/campaigns`,
-  stats: `${backendPath}/campaigns/stats`
+  stats: `${backendPath}/campaigns/stats`,
+  userReward: (campaignId, supporterId, userRewardId) => `${backendPath}/campaigns/${campaignId}/user/${supporterId}/rewards/${userRewardId}`
+}
+
+export const rewards = {
+  getById: (campaignId, rewardId) => `${backendPath}/campaigns/${campaignId}/user/rewards/${rewardId}`,
 }
 
 export const updates = {
