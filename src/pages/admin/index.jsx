f1722df97477fe6f2e27fe50a7b1116353c09911
Augeliua <128767874+Augeliua@users.noreply.github.com>
2025-11-04 21:36:23 +0800

    Update index.jsx

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -47,10 +47,10 @@ export default function Admin() {
             method: 'GET',
             credentials: 'include'
         });
-        // if (!res.ok) {
-        //     toast.error("Unable to fetch campaigns...");
-        //     return;
-        // }
+        if (!res.ok) {
+            toast.error("Unable to fetch campaigns...");
+            return;
+        }
         const data = await res.json();
         console.log(data);
         const totalCampaigns = Object.values(data).reduce((sum, count) => sum + count, 0);
@@ -80,10 +80,10 @@ export default function Admin() {
             method: 'GET',
             credentials: 'include'
         });
-        if (!res.ok) {
-            toast.error("Unable to fetch users...");
-            return;
-        }
+        // if (!res.ok) {
+        //     toast.error("Unable to fetch users...");
+        //     return;
+        // }
         const { users } = await res.json();
         setNoOfUsers(users.length);
     }
