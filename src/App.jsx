79ceb89d0c78b7228758c5a9439abc11fad6d8f8
onncera <onncera@localhost.com>
2025-11-05 08:43:56 +0800

    footer pages for about us and discover

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import TermsAndConditions from './pages/terms';
 import ContactUs from './pages/contact';
 import HowItWorks from './pages/how-it-works';
 import ComingSoon from './pages/coming-soon';
+import Discover from './pages/discover';
+import AboutUs from './pages/about';
 import Campaign from './pages/campaign/CampaignManager';
 import CampaignDetails from './pages/campaign/CampaignDetails';
 import DonationPage from './pages/campaign/components/DonationPage.jsx';
@@ -32,13 +34,11 @@ import RewardProof from './pages/rewards/RewardProof.jsx';
 import Updates from './pages/campaign/updates/index.jsx';
 import UserUpdates from './pages/updates/index.jsx';
 import AuthCallback from './pages/auth/AuthCallback.jsx';
-import RoleSelection from './pages/auth/RoleSelection.jsx';
 import RedeemUserReward from './pages/campaign/rewards/RedeemUserReward.jsx';
 import RegisterShop from "./pages/shop/RegisterShop.jsx";
 import RequireRole from './helpers/RequireRole.jsx';
 import CampaignManager from './pages/campaign/CampaignManager.jsx';
 import UpdateComposer from './pages/campaign/components/UpdateComposer.jsx';
-import MapView from './pages/map/index.jsx';
 
 
 function App() {
@@ -53,17 +53,17 @@ function App() {
               GENERAL + PROFILE (everyone)
              ========================= */}
           <Route path="/" element={<Home searchQuery={searchQuery} />} />
-          <Route path="/map" element={<MapView />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/auth/callback" element={<AuthCallback />} />
-          <Route path="/auth/select-role" element={<RoleSelection />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password" element={<ResetPassword />} />
           <Route path="/terms" element={<TermsAndConditions />} />
           <Route path="/contact" element={<ContactUs />} />
           <Route path="/how-it-works" element={<HowItWorks />} />
           <Route path="/coming-soon" element={<ComingSoon />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/about" element={<AboutUs />} />
           <Route path="/profile" element={<EditProfile />} />
 
           {/* =========================
@@ -133,7 +133,9 @@ function App() {
           <Route
             path="/campaign/:id"
             element={
-              <CampaignDetails />
+              <RequireRole allowedRoles={['BUSINESS_REPRESENTATIVE']}>
+                <CampaignDetails />
+              </RequireRole>
             }
           />
 
@@ -141,7 +143,9 @@ function App() {
           <Route
             path="/campaign/:campaignId/updates/:updateId"
             element={
-              <Updates />
+              <RequireRole allowedRoles={['BUSINESS_REPRESENTATIVE']}>
+                <Updates />
+              </RequireRole>
             }
           />
 
